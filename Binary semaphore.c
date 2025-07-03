dfdafa6a16aac71e9671b2625e701f5a7c195aeb
subavenichandrasekaran11 <subavenichandrasekaran@gmail.com>
2025-07-03 21:59:06 +0530

    Create Binary semaphore.c
    Binary Semaphore:
    Initialized to 1 (resource available).
    When a thread/process accesses it, the value becomes 0 (resource locked).
    Other threads block if the value is 0.
    When the resource is released, the value becomes 1 again.

---
+++ Binary semaphore.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<semaphore.h>
+#include<pthread.h>
+sem_t binarysem;
+void* my_function(void* arg){
+    int id=(int)arg;
+    sem_wait(&binarysem);
+    printf(" entering...thread id:%d\n",id);
+    fflush(stdout); 
+    sleep(5);
+    printf("leaving...thread id:%d\n",id);
+    fflush(stdout); 
+    sem_post(&binarysem);
+    return 0;
+}
+
+int main(){
+    pthread_t t1,t2;
+    int id1=1,id2=2;
+    sem_init(&binarysem,0,1);
+    pthread_create(&t1,NULL,my_function,(void*)&id1);
+    pthread_create(&t2,NULL,my_function,(void*)&id2);
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    sem_destroy(&binarysem);
+    return 0;
+    
+}
+
+
+
+\*output
